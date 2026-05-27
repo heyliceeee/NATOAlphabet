89159bfacc12e6aed943e640c14faae1697a2253
Alice Dias <58340504+heyliceeee@users.noreply.github.com>
2026-05-27 10:23:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import pandas as pd
 
+def generate_phonetic_code(word):
+    """
+    Generate a phonetic code for a given word.
+    """
+    try:
+        result = [phonetic_dict[letter] for letter in word.upper()] # convert a word to a list of the fonetic code
+    except KeyError:
+        print("Invalid word")
+    else:
+        print(f"NATO phonetic code: {word} {result}")
+
 df = pd.read_csv("nato_phonetic_alphabet.csv") # read a csv file
 phonetic_dict = {row.letter: row.code for (_, row) in df.iterrows()} # create a dictionary from csv
 
 word = input("Word: ") # get a word from a user
-
-try:
-    result = [phonetic_dict[letter] for letter in word.upper()] # convert a word to a list of the fonetic code
-    print(f"NATO phonetic code: {word} {result}")
-except KeyError:
-    print("Invalid word")
+generate_phonetic_code(word) # generate a phonetic code
